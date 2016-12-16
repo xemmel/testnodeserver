a6c3d898c95e560b8bc200061b2313757f4c0f71
xemmel <lacour@gmail.com>
2016-12-16 17:59:33 +0100

    Added basic rest func

--- server.js
+++ server.js
@@ -4,6 +4,27 @@ var app = express();
 
 var port = process.env.port || 1234;
 
+var router = express.Router();
+
+router.route('/persons')
+    .get(function(req,res) {
+          res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Cache-Control','public, max-age=200');
+    res.setHeader('Content-Type', 'application/json');
+ 
+    var result = [
+        {id: 1, name: "Morten"},
+        {id: 2, name: "Clara"},
+        {id: 3, name: "Mogens"}
+        
+    ];
+    res.json(result);
+
+    });
+
+app.use('/api',router);
 
 app.get('/',function(req,res) {
     
